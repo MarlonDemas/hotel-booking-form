bc64786b4b772edf99af530f231081604d8759a8
MarlonDemas <demasmarlon@gmail.com>
2019-02-07 23:00:51 +0200

    created the mockup form to test the output with some basic styles

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +7,75 @@
         <title>Hotel Booking Form</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+        <style>
+            .section {
+                background-image: url("pexels-photo-573552.jpeg");
+                background-size: cover;
+                background-repeat: no-repeat;
+                height: 100vh;
+            }
+            hr {
+                background-color: transparent;
+            }
+            .field {
+                padding-bottom: 30px;
+            }
+            .container .columns .column .box {
+                background-color: rgba(0,0,0,0.5);
+            }
+            ::placeholder {
+                color: red !important;
+            }
+        </style>
     </head>
     <body>
+        <section class="section">
+            <h2 class="title is-2 has-text-white" style="margin-left: 5%">Hotel Reservations</h2>
+            <hr>
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-half">
+                        <div class="box">
+                            <form action="index.php" method="post" class="form">
+                                <div class="field">
+                                    <label class="label has-text-white">Name</label>
+                                    <p class="control">
+                                        <input class="input" type="text" placeholder="Please enter your name">
+                                    </p>
+                                </div>
+                                
+                                <div class="field">
+                                    <label class="label has-text-white">Surname</label>
+                                    <p class="control">
+                                        <input class="input" type="text" placeholder="Please enter your surname">
+                                    </p>
+                                </div>
+                                
+                                <div class="field">
+                                <label class="label has-text-white">Select your hotel</label>
+                                <p class="control">
+                                    <span class="select">
+                                    <select>
+                                        <option>Hotel 1</option>
+                                        <option>Hotel 2</option>
+                                        <option>Hotel 3</option>
+                                    </select>
+                                    </span>
+                                </p>
+                                </div>
+
+                                <div class="field" style="padding-bottom: 100px">
+                                    <label class="label has-text-white">Number of days</label>
+                                    <p class="control">
+                                        <input class="input" type placeholder="e.g 3">
+                                    </p>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </body>
 </html>
