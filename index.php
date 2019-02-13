5ac0403d2dd4b1544e253d17e66d3f9125cab1b4
MarlonDemas <demasmarlon@gmail.com>
2019-02-13 09:59:48 +0200

    added different background image

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <style>
         .section {
-                background-image: url("img/pexels-photo-573552.jpeg");
+                background-image: url("img/governor-s-mansion-montgomery-alabama-grand-staircase-161758");
                 background-size: cover;
                 background-repeat: no-repeat;
                 height: 100vh;
