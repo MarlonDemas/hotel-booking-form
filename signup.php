4346f11db1e7eec7d42b14f35acb886cec761cf1
MarlonDemas <demasmarlon@gmail.com>
2019-02-15 10:46:58 +0200

    changed the index page to a login form and added a signup page

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Login</title>
+        <title>Sign Up</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
         <style>
@@ -12,28 +12,16 @@
                 background-size: cover;
                 background-repeat: no-repeat;
             }
-            
-            body .hero .navbar {
-                background: rgba(255,255,255,0.4);
-                padding-bottom: 10px;
-            }
         </style>
     </head>
     <body>
         <section class="hero is-fullheight has-background">
-            <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="https://bulma.io">
-                        <h3 class="title is-4 has-text-weight-bold has-text-black is-uppercase">me&you</h3>
-                    </a>
-                </div>
-            </nav>
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <div class="column is-4 is-offset-4">                        
+                    <div class="column is-4 is-offset-4">                       
                         <div class="box">
-                        <h3 class="title has-text-black">Login</h3>
-                        <p class="subtitle has-text-black">Please login to proceed.</p>
+                        <h3 class="title has-text-black">Sign Up</h3>
+                        <p class="subtitle has-text-black">Please sign up to proceed.</p>
                             <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
                                 <div class="field">
                                     <div class="control">
@@ -46,10 +34,16 @@
                                         <input class="input is-large" name="pass" type="password" placeholder="Your Password">
                                     </div>
                                 </div>
-                                <button name="submit" class="button is-block is-info is-large is-fullwidth">Login</button>
+
+                                <div class="field">
+                                    <div class="control">
+                                        <input class="input is-large" name="cpass" type="password" placeholder="Re-type Password">
+                                    </div>
+                                </div>
+                                <button class="button is-block is-info is-large is-fullwidth" name="submit">Sign Up</button>
                                 <hr>
-                                Don't have an account? 
-                                <a href="signup.php">Sign Up</a>
+                                Already a member? 
+                                <a href="../">Login</a>
                             </form>
                         </div>
                     </div>
