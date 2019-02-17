93bdfc73492783102d992a74c9c9d1c0f7014335
MarlonDemas <demasmarlon@gmail.com>
2019-02-17 16:24:54 +0200

    added cards with information about the selected hotel

--- home.php
+++ home.php
@@ -39,8 +39,14 @@
 
         .section {
             position: absolute;
-            top: 20%;
+            top: 10%;
             left: 15%;
+        }
+        .first, .second, .third {
+                display: none;
+        }
+        .image img {
+            max-height: 290px;
         }
     </style>
 </head>
@@ -75,53 +81,136 @@
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4 has-text-centered">Make Your Booking</h2>
-                        <div class="field">
-                            <label class="label">Select your hotel</label>
-                            <p class="control has-icons-left">
-                                <span class="select">
-                                    <select name="hotelname">
-                                        <option>--please select your hotel--</option>
-                                        <option value="Hotel 1">Hotel 1</option>
-                                        <option value="Hotel 2">Hotel 2</option>
-                                        <option value="Hotel 3">Hotel 3</option>
-                                    </select>
-                                </span>
-                                <span class="icon is-left">
-                                    <i class="fas fa-hotel"></i>
-                                </span>
-                            </p>
-                        </div>
+                        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" class="form">
+                            <div class="field">
+                                <label class="label">Select your hotel</label>
+                                <p class="control has-icons-left">
+                                    <span class="select">
+                                        <select name="hotelname">
+                                            <option value="">--please select your hotel--</option>
+                                            <option value="Hotel 1">Voyage Hotel</option>
+                                            <option value="Hotel 2">Summer Dune Hotel</option>
+                                            <option value="Hotel 3">Sapphire Hotel</option>
+                                        </select>
+                                    </span>
+                                    <span class="icon is-left">
+                                        <i class="fas fa-hotel"></i>
+                                    </span>
+                                </p>
+                            </div>
 
-                        <div class="field">
-                            <label class="label">Check-in Date</label>
-                            <p class="control has-icons-left">
-                                <input class="input" name="date1" type="date" value="<?php echo date('Y-m-d');?>">
-                                <span class="icon is-left">
-                                    <i class="far fa-calendar-alt"></i>
-                                </span>
-                            </p>
-                        </div>
+                            <div class="field">
+                                <label class="label">Check-in Date</label>
+                                <p class="control has-icons-left">
+                                    <input class="input" name="date1" type="date" value="<?php echo date('Y-m-d');?>">
+                                    <span class="icon is-left">
+                                        <i class="far fa-calendar-alt"></i>
+                                    </span>
+                                </p>
+                            </div>
 
-                        <div class="field">
-                            <label class="label">Check-out Date</label>
-                            <p class="control has-icons-left">
-                                <input class="input" name="date2" type="date" value="<?php echo date('Y-m-d'); ?>">
-                                <span class="icon is-left">
-                                    <i class="far fa-calendar-alt"></i>
-                                </span>
-                            </p>
-                        </div>
+                            <div class="field">
+                                <label class="label">Check-out Date</label>
+                                <p class="control has-icons-left">
+                                    <input class="input" name="date2" type="date" value="<?php echo date('Y-m-d'); ?>">
+                                    <span class="icon is-left">
+                                        <i class="far fa-calendar-alt"></i>
+                                    </span>
+                                </p>
+                            </div>
 
-                        <div class="field">
-                            <label class="label">No. of Guests</label>
-                            <p class="control has-icons-left">
-                                <input class="input" name="date2" type="number">
-                            </p>
-                        </div>
+                            <div class="field">
+                                <label class="label">No. of Guests</label>
+                                <p class="control">
+                                    <input class="input" name="date2" type="number" min="1" max="15" value="1">
+                                </p>
+                            </div>
+                            
+                            <div class="field">
+                                <label class="label">No. of Rooms</label>
+                                <p class="control">
+                                    <input class="input" name="date2" type="number" min="1" max="5" value="1">
+                                </p>
+                            </div>
 
-                        <div class="field is-grouped is-centered">
-                            <div class="control">
-                                <button class="button is-link" name="submit">Submit Booking</button>
+                            <div class="field is-grouped">
+                                <div class="control">
+                                    <button class="button is-link" name="submit">Submit Booking</button>
+                                </div>
+                            </div>
+                    </div>
+                    </form>
+                </div>
+                <div class="column is-one-third">
+                    <div class="card first">
+                        <div class="card-image">
+                            <figure class="image">
+                                <img src="img\pexels-photo-262047.jpeg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4">Voyage Hotel</p>
+                                </div>
+                                <div class="media-right">
+                                    <p>R250 per person</p>
+                                </div>
+                            </div>
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Animi quia nesciunt tempora vero neque pariatur culpa error cum quo eos, impedit
+                                magnam! Aspernatur consectetur ipsa quibusdam sint excepturi eligendi pariatur.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card second">
+                        <div class="card-image">
+                            <figure class="image">
+                                <img src="img\pexels-photo-261102.jpeg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4">Summer Dune Hotel</p>
+                                </div>
+                                <div class="media-right">
+                                    <p>R290 per person</p>
+                                </div>
+                            </div>
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Animi quia nesciunt tempora vero neque pariatur culpa error cum quo eos, impedit
+                                magnam! Aspernatur consectetur ipsa quibusdam sint excepturi eligendi pariatur.
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="card third">
+                        <div class="card-image">
+                            <figure class="image">
+                                <img src="img\governor-s-mansion-montgomery-alabama-grand-staircase-161758.jpeg" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4">Sapphire Hotel</p>
+                                </div>
+                                <div class="media-right">
+                                    <p>R330 per person</p>
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Animi quia nesciunt tempora vero neque pariatur culpa error cum quo eos, impedit
+                                magnam! Aspernatur consectetur ipsa quibusdam sint excepturi eligendi pariatur.
                             </div>
                         </div>
                     </div>
@@ -129,6 +218,32 @@
             </div>
         </div>
     </section>
+    <!-- jQuery JS 3.1.0 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script>
+        // var selectedItem = sessionStorage.getItem("SelectedItem");
+        // $('#dropdown').val(selectedItem);
+        $('select').change(function () {
+            var sel = $('select option:selected');
+            if (sel.val() == "Hotel 1") {
+                $('.first').css('display', "block");
+                $('.second').css('display', "none");
+                $('.third').css('display', "none");
+            } else if (sel.val() == "Hotel 2") {
+                $('.first').css('display', "none");
+                $('.second').css('display', "block");
+                $('.third').css('display', "none");
+            } else if (sel.val() == "Hotel 3") {
+                $('.first').css('display', "none");
+                $('.second').css('display', "none");
+                $('.third').css('display', "block");
+            } else if (sel.val() == "") {
+                $('.first').css('display', "none");
+                $('.second').css('display', "none");
+                $('.third').css('display', "none");
+            }
+        });
+    </script>
 </body>
 
 </html>
