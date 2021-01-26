6e72791144c2dcedbb540dcc4b4c5da24a0013b9
adeptmarlon <marlon.d@adept.co.za>
2021-01-26 17:45:19 +0200

    updated user for mysql login

--- config/user.php
+++ config/user.php
@@ -1,6 +1,6 @@
 <?php
     // Defining global variables
-    define('DB_HOST', '5ea19cba@us-cdbr-east-03.cleardb.com');
+    define('DB_HOST', 'us-cdbr-east-03.cleardb.com');
     define('DB_USER', 'b03acd4c826063');
     define('DB_PASS', '5ea19cba');
     define('DB_NAME', 'heroku_3fa2a2cdf7b375a');
